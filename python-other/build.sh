015c1c7ce1139dbb0729891eeef597b4c2458336
Heiko W. Rupp <hwr@pilhuhn.de>
2025-11-13 11:37:35 +0100

    Determine the arch tag from uname

--- python-other/build.sh
+++ python-other/build.sh
@@ -1,4 +1,27 @@
-podman build -t quay.io/pilhuhn/py-simple-http .
+#!/bin/sh
+
+xarch=`uname -m`
+case $xarch in
+	'arm64')
+	arch='arm64'
+	;;
+	
+	'aarch64')
+	arch='arm64'
+	;;
+
+	'x86_64')
+	arch='amd64'
+	;;
+
+	*)
+	echo "Unknown architecture $xarch"
+	exit 1
+esac
+
+podman build -t quay.io/pilhuhn/py-simple-http:$arch --platform linux/$arch .
+podman push quay.io/pilhuhn/py-simple-http:$arch 
+
 
 
 #podman manifest create quay.io/pilhuhn/py-simple-http:multi
